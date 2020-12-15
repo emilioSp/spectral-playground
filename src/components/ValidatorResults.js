03d7d36bb53c879ad53583e8a5bdf5f1e0f65249
emilioSp <emilio.spatola@gmail.com>
2020-12-15 12:21:59 +0100

    wip

--- src/components/ValidatorResults.js
+++ src/components/ValidatorResults.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
+import { Badge } from 'design-react-kit';
 
 const useStyle = createUseStyles({
   cursorPointer: {
     cursor: 'pointer'
   },
-  cellCenterText: {
+  center: {
     textAlign: 'center'
   },
-  cellBreakWords: {
+  breakWords: {
     wordBreak: 'break-all'
   }
 });
@@ -17,9 +18,30 @@ export const ValidatorResults = props => {
   if (props.isValidating) return 'Validation in progress...';
   if (!props.results) return null;
 
+  const errors = props.results.filter(r => r.severity === 1);
+  const warnings = props.results.filter(r => r.severity !== 1);
+
   const classes = useStyle();
 
-  return <div className="table-responsive-md">
+  return <>
+      <h4 className={classes.center}>
+        <span>Trovati </span>
+      <Badge
+        color="danger"
+        pill={false}
+        tag="span"
+      >
+        {errors.length} errori
+      </Badge> <span> e </span>
+        <Badge
+          color="warning"
+          pill={false}
+          tag="span"
+        >
+          {warnings.length} warnings
+        </Badge>
+    </h4>
+  <div className="table-responsive-md">
     <table className="table align-content-center">
       <thead>
       <tr>
@@ -32,12 +54,13 @@ export const ValidatorResults = props => {
       {
         props.results.map(r =>
       <tr className={classes.cursorPointer} key={r.fingerprint} onClick={() => props.onResultClick({ line: r.range.start.line, character: r.range.start.character })}>
-        <td className={classes.cellCenterText}>{r.severity}</td>
-        <td className={classes.cellCenterText}>{r.range.start.line}</td>
-        <td className={classes.cellBreakWords}>{r.message}</td>
+        <td className={classes.center}>{r.severity}</td>
+        <td className={classes.center}>{r.range.start.line}</td>
+        <td className={classes.breakWords}>{r.message}</td>
       </tr>)
       }
       </tbody>
     </table>
   </div>
+    </>
 }
