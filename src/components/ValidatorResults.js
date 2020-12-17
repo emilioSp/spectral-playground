4a7a76d6f5a2e801a05698cb840d743912f1cab1
emilioSp <emilio.spatola@gmail.com>
2020-12-17 13:35:35 +0100

    wip

--- src/components/ValidatorResults.js
+++ src/components/ValidatorResults.js
@@ -37,26 +37,4 @@ export const ValidatorResults = props => {
       }
     </div>
     </>
-
-  // return <div className={`table-responsive-md ${classes.enableScrollResults}`}>
-  //       <table className="table align-content-center">
-  //         <thead>
-  //         <tr>
-  //           <th>Type</th>
-  //           <th>Line</th>
-  //           <th>Message</th>
-  //         </tr>
-  //         </thead>
-  //         <tbody>
-  //         {
-  //           props.results.map(r =>
-  //         <tr role="button" key={r.fingerprint} onClick={() => props.onResultClick({ line: r.range.start.line, character: r.range.start.character })}>
-  //           <td className="text-center">{r.severity}</td>
-  //           <td className="text-center">{r.range.start.line}</td>
-  //           <td className={classes.breakWords}>{r.message}</td>
-  //         </tr>)
-  //         }
-  //         </tbody>
-  //       </table>
-  //     </div>
 }
